77094ffd71679d9760948dc3c146327fc1783ffb
jard345 <jrodriguezd16@miumg.edu.gt>
2024-03-02 10:01:39 -0600

    ejercicio 2.1 realizado por José Rodriguez 0909-23-5430

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a =-1,b =0;
+    cout<<"Numeros impares"<<endl;
+    while (a<49){
+        a+=2;
+        cout<<a<<endl;
+    }
+    cout<<"Numeros pares"<<endl;
+    while (b<50){
+        b+=2;
+        cout<<b<<endl;
+    }
+
+    return 0;
+}
