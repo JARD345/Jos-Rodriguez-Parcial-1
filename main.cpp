c99e8402582131b10dd1e6ec4ccdd603cccf8fd1
jard345 <jrodriguezd16@miumg.edu.gt>
2024-03-02 10:09:57 -0600

    ejercicio 2.2 realizado por José Rodriguez 0909-23-5430

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
+    /*
     int a =-1,b =0;
     cout<<"Numeros impares"<<endl;
     while (a<49){
@@ -12,6 +13,10 @@ int main() {
         b+=2;
         cout<<b<<endl;
     }
-
+*/
+    int i;
+    for (i=0;i<=31;i+=3){
+        cout<<i<<endl;
+    }
     return 0;
 }
